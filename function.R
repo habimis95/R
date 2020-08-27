61a177c30954ecdb9edd5b51cabbaf59e1f52d2a
habimis95 <50472059+habimis95@users.noreply.github.com>
2020-08-27 15:45:37 +0700

    update

--- function.R
+++ function.R
@@ -1,3 +1,27 @@
+basic_calculations.function = function(a,b){
+a = as.integer(readline(prompt='In put a: '))
+b = as.integer(readline(prompt='In put b: '))
+tong = a + b 
+hieu = a - b
+tich = a * b
+thuong = a/b
+
+print(paste('Tong =', tong))
+print(paste('Hieu =', hieu))
+print(paste('Tich =', tich))
+print(paste('Thuong =', thuong))
+
+}
+
+odd_even.function = function(x){
+x = as.numericreadline(prompt='Input x: ')
+if(x%%2==0){
+    print(paste(x, 'is even number'))
+}else{
+    print(paste(x, 'is odd number'))
+}
+    }
+
 sum_even.function = function(start,end){
     sum_even = 0
     for(i in start:end){
@@ -12,7 +36,7 @@ drink.function = function(price, type='Tea'){
     print(paste('With', price, ', you can drink', type))
 }
 
-tien_dien.function = function(tien_dien,so_kw){
+electricity_bill.function = function(tien_dien,so_kw){
 muc1=1678
 muc2=1734
 muc3=2014
@@ -41,3 +65,60 @@ if(so_kw<=50){
     tien_dien=bac50*muc1+bac50*muc2+bac100*muc3+bac100*muc4+bac100*muc5(so_kw - bac50 - bac50 - bac100 - bac100 - bac100)*muc6
 }
     }
+    
+hotel_bill.function= function(ma_so,so_ngay){
+vip1=1000000
+vip2=900000
+vip3=850000
+standard1=700000
+standard2=550000
+    
+ma_so=as.integer(readline(prompt='Nhap ma so (1/2/3/4/5): '))
+so_ngay = as.integer(readline(prompt='Nhap so ngay: '))
+    
+don_gia_phong = switch(ma_so, vip1,vip2,vip3,standard1,standard2)
+    
+tien = 0 
+if(so_ngay<3){
+    tien=don_gia_phong*so_ngay
+}else if(so_ngay<7){
+    tien=don_gia_phong*so_ngay*0.9
+}else{
+    tien=don_gia_phong*so_ngay*0.8
+}
+         print(paste('Tien phai thanh toan: ',tien,'vnd'))
+}
+    
+delivery_bill.function(chon_loai,sokm){
+toi_thieu_2km_bike=10000
+km_bike=3600
+
+toi_thieu_2km_send = 15000
+km_send = 4000
+    
+chon_loai = as.integer(readline(prompt='Nhap 1 chon GO-BIKE, 2 chon GO-SEND: '))
+
+sokm = as.numeric(readline(prompt='So km:'))
+    
+tien_xe = 0 
+if(chon_loai==1){
+    if(sokm<2){
+    tien_xe=toi_thieu_2km_bike
+    }else{
+    tien_xe=toi_thieu_2km_bike  + (sokm - 2)*km_bike
+    }
+}else if(chon_loai==2){
+    if(sokm<2){
+        tien_xe = toi_thieu_2km_send
+    }else{
+        tien_xe = toi_thieu_2km_send + (sokm - 2)*km_send
+    }
+}else {
+    print('Chi co 2 loai la GO-BIKE va GO-SEND')
+}
+
+print(paste('Tien xe:', tien_xe, 'VND'))
+chuoi_tien = paste(format(tien_xe, big.mark='.', decimal.mark=','),sep='')
+
+print(paste('Tien xe: ', chuoi_tien,'VND'))
+}
